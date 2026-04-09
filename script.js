bc264955fa48c23661ed8a4651c5f4c08344ee25
aryan740 <aryangoyat2004@gmail.com>
2026-04-09 20:59:11 +0530

    Do some minor updates in UI

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function analyzeRisk() {
             };
 
             try {
-                const response = await fetch('http://127.0.0.1:8000/predict', {
+                const response = await fetch('https://aryan7004-traffic-risk-api.hf.space/predict', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify(payload)
